94fa3c95c0d94bc6b624ca14de1ba1d0d5ce8ca2
malep2007 <ephraim.malinga@gmail.com>
2016-09-22 11:10:44 +0300

    Add files via upload

---
+++ SignalTransmit.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial = SoftwareSerial(0,1);
+int counter = 0;
+void setup()
+{
+  String msg = "AT";
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Sending message to serial port");
+}
+
+void loop()
+{
+  Serial.println("Start");
+  delay(1000);
+  
+  //delay(3000);
+}
+
